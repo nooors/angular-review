17057280fd6dd32524205a0cb469bf7fac2cb0ba
Jose Nortes <nooors@gmail.com>
2022-05-29 11:06:32 +0200

    http reponse implemented with observables, refactor-> template reference/ refactor-> async pipe

--- src/app/countries-component/countries-component.component.ts
+++ src/app/countries-component/countries-component.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { RESTCoutriesService } from "../services/restcoutries.service";
+import { map } from "rxjs/operators";
+import { EMPTY, Observable } from "rxjs";
 
 @Component({
   selector: "app-countries-component",
@@ -8,7 +10,11 @@ import { RESTCoutriesService } from "../services/restcoutries.service";
       <h2 class="title">Countries</h2>
     </div>
     <div class="data">
-      <form class="country">
+      <!-- Refactor -->
+      <!-- #country is a template reference vealue -->
+      <input #country /><br />
+      <button (click)="showCapital(country.value)">Find the Capital</button>
+      <!-- <form class="country">
         <label for="country">Search for a capital</label>
         <input
           type="text"
@@ -17,21 +23,50 @@ import { RESTCoutriesService } from "../services/restcoutries.service";
           placeholder="country"
         />
         <button type="button" (click)="showCapital()">Search</button>
-      </form>
-      <div class="capital">{{ capital }}</div>
+      </form> -->
+
+      <!-- async pipes works with promises observables and handles susbscription/unsubscription -->
+      <!-- <div class="capital">{{ capital }}</div> -->
+      <div class="capital">{{ capital$ | async }}</div>
+      <!-- Best practice |^ -->
     </div>
   `,
   styleUrls: ["./countries-component.component.sass"],
 })
 export class CountriesComponentComponent implements OnInit {
-  countryName = "";
-  capital = "";
+  // countryName = "";
+
+  // Refactor using async pipe
+  // capital = "";
+  capital$: Observable<string> = EMPTY;
   constructor(private countriesServices: RESTCoutriesService) {}
 
-  showCapital() {
-    this.countriesServices
-      .getCountry(this.countryName)
-      .subscribe((data: any[]) => (this.capital = data[0].capital));
+  // showCapital() {
+  //   this.countriesServices
+  //     .getCountry(this.countryName)
+  //     .subscribe((data: any[]) => (this.capital = data[0].capital));
+  // }
+
+  // Refactor
+  // showCapital(countryName: string) {
+  //   this.countriesServices
+  //     .getCountry(countryName)
+  //     .subscribe((data: any[]) => (this.capital = data[0].capital));
+  // }
+
+  // Refactor using map
+  // showCapital(countryName: string) {
+  //   this.countriesServices
+  //     .getCountry(countryName)
+  //     .pipe(map((data) => data[0].capital))
+  //     .subscribe((capital) => (this.capital = capital));
+  // }
+
+  // Refactor using async pipe
+  showCapital(countryName: string) {
+    this.capital$ = this.countriesServices
+      .getCountry(countryName)
+      .pipe(map((data) => data[0].capital));
   }
 
   ngOnInit(): void {}
